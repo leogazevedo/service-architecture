fe51f583e0f965c8f865ce8c6615ce1f3eccb773
Leonardo Guerreiro Azevedo <leogazevedo@gmail.com>
2026-03-15 19:31:32 -0300

    feat: implemented error handler.

--- restful_with_flask/tasks_app.py
+++ restful_with_flask/tasks_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 HOST = "127.0.0.1"
@@ -22,6 +22,18 @@ tasks = [
 @app.route("/todo/api/tasks", methods=["GET"])
 def get_tasks():
     return jsonify({"tasks": tasks})
+
+@app.route("/todo/api/tasks/<int:task_id>", methods=["GET"])
+def get_task(task_id):
+    task = [task for task in tasks if task["id"] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({"task": task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=True)
