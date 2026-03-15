238407cd03d904cec244cbf5b6fffc6683a8ded6
Leonardo Guerreiro Azevedo <leogazevedo@gmail.com>
2026-03-15 19:42:24 -0300

    feat: implemented method to add a task.

--- restful_with_flask/tasks_app.py
+++ restful_with_flask/tasks_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 HOST = "127.0.0.1"
@@ -34,6 +34,18 @@ def get_task(task_id):
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
+@app.route("/todo/api/tasks", methods=["POST"])
+def create_task():
+    if not request.json or not "title" in request.json:
+        abort(400)
+    task = {
+        "id": tasks[-1]["id"] + 1,
+        "title": request.json["title"],
+        "description": request.json.get("description", ""),
+        "done": False
+    }
+    tasks.append(task)
+    return jsonify({"task": task}), 201
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=True)
